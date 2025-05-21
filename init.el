c661855828e028924d366cde89efee835a1f7d58
谛叶 <GlitchLord00011010@gmail.com>
2025-05-21 23:10:07 +0800

    Update init.el

--- init.el
+++ init.el
@@ -36,13 +36,13 @@
 
 
 
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
-(require 'eaf)
-(require 'eaf-pdf-viewer)
-(require 'eaf-browser)
-(require 'eaf-music-player)
-(require 'eaf-video-player)
-(require 'eaf-image-viewer)
+;(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+;(require 'eaf)
+;(require 'eaf-pdf-viewer)
+;(require 'eaf-browser)
+;(require 'eaf-music-player)
+;(require 'eaf-video-player)
+;(require 'eaf-image-viewer)
 
 
 (when (file-exists-p custom-file)
